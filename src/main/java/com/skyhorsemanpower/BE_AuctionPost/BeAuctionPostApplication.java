e71936f24ae31ec36f4b04fa4ef590a440c70b8a
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-05 17:09:49 +0900

    First commit

---
+++ src/main/java/com/skyhorsemanpower/BE_AuctionPost/BeAuctionPostApplication.java
@@ -0,0 +1,13 @@
+package com.skyhorsemanpower.BE_AuctionPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAuctionPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAuctionPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyhorsemanpower/BE_AuctionPost/BeAuctionPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyhorsemanpower.BE_AuctionPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAuctionPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
