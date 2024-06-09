519fa9a687ee3fbc4faa504c76278ee8ea3a2d2d
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-10 00:59:11 +0900

    feat: 경매 비정상 종료 상태 추가(#12)

--- src/main/java/com/skyhorsemanpower/BE_AuctionPost/status/AuctionStateEnum.java
+++ src/main/java/com/skyhorsemanpower/BE_AuctionPost/status/AuctionStateEnum.java
@@ -10,8 +10,10 @@ public enum AuctionStateEnum {
     BEFORE_AUCTION,
     // 경매 진행 중
     AUCTION_IS_IN_PROGRESS,
-    // 경매 정상 마감
+    // 경매 정상 종료
     AUCTION_NORMAL_CLOSING,
     // 경매 참여자 없음
     AUCTION_NO_PARTICIPANTS,
+    // 경매 비정상 종료
+    AUCTION_ABNORMAL_CLOSING
 }
