60fb36771f94f1d71c51f699032a170902782c43
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-12 01:19:24 +0900

    refactor: 인플루언서명, 제목 컬럼 길이 증가

--- src/main/java/com/skyhorsemanpower/BE_AuctionPost/domain/cqrs/command/CommandAuctionPost.java
+++ src/main/java/com/skyhorsemanpower/BE_AuctionPost/domain/cqrs/command/CommandAuctionPost.java
@@ -29,10 +29,10 @@ public class CommandAuctionPost extends BaseCreateAndEndTimeEntity {
     @Column(nullable = false, length = 50)
     private String influencerUuid;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 100)
     private String influencerName;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String title;
 
     @Column(nullable = false, length = 1000)
@@ -56,13 +56,13 @@ public class CommandAuctionPost extends BaseCreateAndEndTimeEntity {
     @Column(nullable = false, length = 30)
     private LocalDateTime auctionStartTime;
 
-    @Column(nullable = false, precision = 10, scale = 2)
+    @Column(nullable = false, precision = 20, scale = 2)
     private BigDecimal startPrice;
 
-    @Column(nullable = false, precision = 10, scale = 2)
+    @Column(nullable = false, precision = 20, scale = 2)
     private BigDecimal incrementUnit;
 
-    @Column(nullable = true, precision = 10, scale = 2)
+    @Column(nullable = true, precision = 20, scale = 2)
     private BigDecimal totalDonation;
 
     @Column(nullable = false)
