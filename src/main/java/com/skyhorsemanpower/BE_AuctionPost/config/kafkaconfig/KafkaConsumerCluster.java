b58efe743ccb2ff73094f2eaee989fd3c52b3f0f
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-19 21:26:54 +0900

    feat: kafka 메세지 받은 후 경매글 내용 업데이트(#80)

---
+++ src/main/java/com/skyhorsemanpower/BE_AuctionPost/config/kafkaconfig/KafkaConsumerCluster.java
@@ -0,0 +1,42 @@
+package com.skyhorsemanpower.BE_AuctionPost.config.kafkaconfig;
+
+import com.skyhorsemanpower.BE_AuctionPost.application.AuctionPostService;
+import com.skyhorsemanpower.BE_AuctionPost.data.vo.UpdateTotalDonationUpdateVo;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaConsumerCluster {
+
+	private final AuctionPostService auctionPostService;
+
+	@KafkaListener(
+		topics = "donation-auction-post-update-topic",
+		groupId = "${spring.kafka.consumer.group-id}"
+	)
+	public void updateDonationAuctionPost(
+		@Payload LinkedHashMap<String, Object> message,
+		@Headers MessageHeaders headers
+	) {
+		log.info("consumer: success >>> message: {}, headers: {}", message.toString(), headers);
+
+		UpdateTotalDonationUpdateVo updateTotalDonationUpdateVo = UpdateTotalDonationUpdateVo.builder()
+			.auctionUuid(message.get("auctionUuid").toString())
+			.totalDonationAmount(BigDecimal.valueOf((Integer) message.get("totalDonationAmount")))
+			.build();
+
+		log.info("consumer: success >>> updateTotalDonationUpdateVo: {}",
+			updateTotalDonationUpdateVo.toString());
+
+		auctionPostService.updateTotalDonationAmount(updateTotalDonationUpdateVo);
+	}
+}
