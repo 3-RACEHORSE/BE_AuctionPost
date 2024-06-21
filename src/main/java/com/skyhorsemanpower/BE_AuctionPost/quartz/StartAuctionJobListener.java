3d41bead840a1e97ad7d086c498a7a5782239d17
ddolboghi <rhdfyd128@gmail.com>
2024-06-21 23:40:26 +0900

    fix: jobListener 로직 제거

--- src/main/java/com/skyhorsemanpower/BE_AuctionPost/quartz/StartAuctionJobListener.java
+++ src/main/java/com/skyhorsemanpower/BE_AuctionPost/quartz/StartAuctionJobListener.java
@@ -32,37 +32,6 @@ public class StartAuctionJobListener implements JobListener {
 
     @Override
     public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobExecutionException) {
-        log.info(">>>>> jobListener");
-        //job 실행이 실패했고 auctionPost가 존재할때 강제로 state 갱신
-        if (jobExecutionException != null && context.getJobDetail().getJobDataMap().get("auctionPost") != null) {
-            CommandAuctionPost commandAuctionPost = (CommandAuctionPost) context.getJobDetail().getJobDataMap().get("auctionPost");
-            auctionPostStateChange(commandAuctionPost);
-        }
+        // job 실행 후
     }
-
-    private void auctionPostStateChange(CommandAuctionPost commandAuctionPost) {
-        // 현재 시간 이후라면 auctionUuid에 해당하는 경매글의 state가 BEFORE_AUCTION이면 AUCTION_IS_IN_PROGRESS로 업데이트하기
-        commandAuctionPostRepository.save(CommandAuctionPost.builder()
-            .auctionPostId(commandAuctionPost.getAuctionPostId())
-            .auctionUuid(commandAuctionPost.getAuctionUuid())
-            .auctionUuid(commandAuctionPost.getAuctionUuid())
-            .adminUuid(commandAuctionPost.getAdminUuid())
-            .influencerUuid(commandAuctionPost.getInfluencerUuid())
-            .influencerName(commandAuctionPost.getInfluencerName())
-            .title(commandAuctionPost.getTitle())
-            .content(commandAuctionPost.getContent())
-            .numberOfEventParticipants(commandAuctionPost.getNumberOfEventParticipants())
-            .localName(commandAuctionPost.getLocalName())
-            .eventPlace(commandAuctionPost.getEventPlace())
-            .eventStartTime(commandAuctionPost.getEventStartTime())
-            .eventCloseTime(commandAuctionPost.getEventCloseTime())
-            .auctionStartTime(commandAuctionPost.getAuctionStartTime())
-            .auctionEndTime(commandAuctionPost.getAuctionEndTime())
-            .startPrice(commandAuctionPost.getStartPrice())
-            .incrementUnit(commandAuctionPost.getIncrementUnit())
-            .totalDonation(commandAuctionPost.getTotalDonation())
-            .state(AuctionStateEnum.AUCTION_IS_IN_PROGRESS)
-            .build());
-    }
-
 }
